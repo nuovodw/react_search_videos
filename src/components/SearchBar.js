cd13ba0a73de94d49644b0c2e32fa7c5bb842213
Daniel Won <dan.j.won@gmail.com>
2021-10-26 13:08:33 -0500

    add event handlers and edit form submission

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,37 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+	state = { term: '' };
+	// constructor(props) {
+	// 	super(props);
+	// 	this.state = { term: '' };
+	// }
+
+	onInputChange = (e) => {
+		this.setState({ term: e.target.value });
+	};
+
+	onFormSubmit = (e) => {
+		e.preventDefault();
+
+		//TODO: Make sure we call callback from parent component
+	};
+
 	render() {
-		return <div className="">SearchBar</div>;
+		return (
+			<div className="search-bar ui segment">
+				<form className="ui form" onSubmit={this.onFormSubmit}>
+					<div className="field">
+						<label>Video Search</label>
+						<input
+							type="text"
+							value={this.state.term}
+							onChange={this.onInputChange}
+						/>
+					</div>
+				</form>
+			</div>
+		);
 	}
 }
 
