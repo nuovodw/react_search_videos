06fda35f4e73ff79e171db2cbec46f5bbdb90e1e
Daniel Won <dan.j.won@gmail.com>
2021-10-27 20:47:38 -0500

    add styling to thumbnail and img title

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 const VideoItem = ({ video }) => {
 	return (
-		<div>
-			<img src={video.snippet.thumbnails.medium.url} alt="video's thumbnail" />
-			{video.snippet.title}
+		<div className={'item'}>
+			<img
+				className="ui image"
+				src={video.snippet.thumbnails.medium.url}
+				alt="video's thumbnail"
+			/>
+			<div className="content">
+				<div className="header">{video.snippet.title}</div>
+			</div>
 		</div>
 	);
 };
